512f3f58569847002936eb1a859630698ee47e2d
nsdigirolamo <nsdigirolamo@gmail.com>
2023-04-25 19:35:04 -0400

    Broke main function down into smaller functions.

--- answer.py
+++ answer.py
@@ -4,7 +4,7 @@ CISC320 Lesson 22 - Devouring Doggos
 April 21, 2023
 """
 
-def process_lines(lines: list[str]) -> list[dict]:
+def process_lines (lines: list[str]) -> list[dict]:
     items = []
     for line in lines:
         info = line.split()
@@ -15,20 +15,9 @@ def process_lines(lines: list[str]) -> list[dict]:
         })
     return items
 
-if __name__ == "__main__":
+def generate_value_table (items: list[dict], row_count: int, col_count: int) -> list[int]:
 
-    # filename = input()
-    filename = "input.txt"
-    with open(filename) as f: lines = f.readlines()
-
-    count = int(lines.pop(0).strip())
-    max_capacity = int(lines.pop(0).strip())
-    items = process_lines(lines)
-
-    row_count = count
-    col_count = max_capacity + 1
-
-    value_table = [[0 for i in range(max_capacity + 1)] for j in range(count)]
+    value_table = [[0 for i in range(col_count)] for j in range(row_count)]
 
     for row in range(0, row_count):
         for col in range(0, col_count):
@@ -47,13 +36,14 @@ if __name__ == "__main__":
                     current_value = value_table[row - 1][col - item["weight"]] + item["value"]
                     value_table[row][col] = max(prev_value, current_value)
 
-    for i in range(0, count):
-        print(f"{'Name: ' + items[i]['name']:<20} {'Weight: ' + str(items[i]['weight']):<15} {'Value: ' + str(items[i]['value']):<15} {value_table[i]}")
+    return value_table
+
+def select_items (items: list[dict], table: list[int]) -> list[dict]:
 
     selected_items = []
 
-    row = row_count - 1
-    col = col_count - 1
+    row = len(table) - 1
+    col = len(table[row]) - 1
     value = value_table[row][col]
 
     while value != 0:
@@ -63,6 +53,33 @@ if __name__ == "__main__":
             value = value - selected["value"]
             col = col - selected["value"]
         row = row - 1
+
+    return selected_items
+
+def print_table (items: list[dict], table: list[int]) -> None:
+    for i in range(0, len(items)):
+        prefix = f"{'Name: ' + items[i]['name']:<25}"
+        prefix += f"{'Weight: ' + str(items[i]['weight']):<15}"
+        prefix += f"{'Value: ' + str(items[i]['value']):<15} "
+        print(prefix + f"{value_table[i]}")
+
+if __name__ == "__main__":
+
+    # filename = input()
+    filename = "input.txt"
+    with open(filename) as f: lines = f.readlines()
+
+    count = int(lines.pop(0).strip())
+    max_capacity = int(lines.pop(0).strip())
+    items = process_lines(lines)
+
+    row_count = count
+    col_count = max_capacity + 1
+    value_table = generate_value_table(items, row_count, col_count)
+
+    print_table(items, value_table)
+
+    selected_items = select_items(items, value_table)
 
     for item in selected_items:
         print(f"{item['name']}")
